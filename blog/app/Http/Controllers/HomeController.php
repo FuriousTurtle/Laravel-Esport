a10e1adfc1310e362352fcda23abd277f6d9e739
Pierrick <marconnet42210@gmail.fr>
2019-04-05 14:06:17 +0200

    First controller

--- blog/app/Http/Controllers/HomeController.php
+++ blog/app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use DB;
+
 class HomeController extends Controller
 {
     /**
@@ -28,6 +30,9 @@ class HomeController extends Controller
         return view('components.accueil.accueil');
     }
     public function schedule(){
-        return view('components.schedule.schedule');
+        /*$BDD = new PDO('mysql:host=localhost;dbname=e-sport', 'root2', 'azerty');*/
+        $result = DB::select("SELECT team.equip_name, score_team1, score_team2 FROM result INNER JOIN team ON result.team_id_1 = team.id OR result.team_id_2 = team.id WHERE result.score_team1 = 'TBD' AND result.score_team2 = 'TBD'");
+        /*$result->fetchAll();*/
+        return view('components.schedule.schedule', ['result' => $result] );
     }
 }
