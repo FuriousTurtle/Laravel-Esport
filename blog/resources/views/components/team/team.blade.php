47983a4a34151d0128ab6c1a768486921748cb44
Pierrick <marconnet42210@gmail.fr>
2019-04-10 10:07:58 +0200

    Nouveau push

--- blog/resources/views/components/team/team.blade.php
+++ blog/resources/views/components/team/team.blade.php
@@ -1,20 +1,17 @@
 @extends('layouts.webtemplate')
-<?php
-$composant = 'team';
-$dir = '../resources/views/components/' . $composant . '/';
-
-?>
 
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/teamstyle.css') }}">
 @endsection
 
 @section('js')
-<script type="text/javascript" src="<?=$dir . $composant?>script.js"></script>
+<script type="text/javascript" src="{{ asset('js/teamscript.js"}}></script>
 @endsection
 
 @section('html')
 <?php
+
 print_r($team);
+
 ?>
 @endsection
